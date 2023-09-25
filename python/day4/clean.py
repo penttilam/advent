b479cd1e818ec4f55a365c0d10e767ab6ab9eef1
mpenttila <michael.penttila@gmail.com>
2023-09-25 14:10:28 -0700

    day 4 part 1 done

---
+++ python/day4/clean.py
@@ -0,0 +1,11 @@
+import sys
+
+with open('list.txt', 'r', encoding='utf-8') as f:
+    overlaps = 0
+    for line in f:
+        pair1, pair2 =[cSet.split('-') for cSet in line.strip().split(',')]
+        if int(pair1[0]) <= int(pair2[0]) and int(pair2[1]) <= int(pair1[1]):
+            overlaps += 1
+        elif int(pair2[0]) <= int(pair1[0]) and int(pair1[1]) <= int(pair2[1]):
+            overlaps += 1
+print(f'Total Contained Sets: {overlaps}')
