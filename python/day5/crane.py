590f40bd4da013a132386e700041b1206e02c9ed
mpenttila <michael.penttila@gmail.com>
2023-09-26 17:59:47 -0700

    day5 part 2 complete

--- python/day5/crane.py
+++ python/day5/crane.py
@@ -1,11 +1,6 @@
 import sys
 
-
-
 with open('data.txt', 'r', encoding='utf-8') as f:
-    #create stacks
-    #read in lines
-    #determine 
 
     stackData = []
     moveData = []
@@ -24,18 +19,22 @@ while stackData:
         crate = row.pop(0)
         if crate != ' ':
             pile.append(crate)
-
+'''
 for line in moveData:
     move = line.strip().split(' ')
-
-#    print(range(int(move[1])))
     for count in range(int(move[1])):
-        #print((move[1]))
         crate = crateStack[int(move[3])-1].pop()
         crateStack[int(move[5])-1].append(crate)
-        #print(crateStack[int(move[5])-1], end='')
-        #print(crateStack[int(move[3])-1])
-#        crateStack[int(move[5])-1].append(crateStack.pop(int(move[3])-1))
+'''
+for line in moveData:
+    crates = []
+    move = line.strip().split(' ')
+    for count in range(int(move[1])):
+        crate = crateStack[int(move[3])-1].pop()
+        crates.append(crate)
+    for count in range(len(crates)):
+        crate = crates.pop() 
+        crateStack[int(move[5])-1].append(crate)
 
 stack = 1
 print(f'Top crates: ', end='')
@@ -43,21 +42,3 @@ for row in crateStack:
     print(f'{stack}:{row.pop()} ', end='')
     stack += 1
 print()
-#print(stackData)
-#print(list(stackMap))
-#for stack in stackMap:
-#    print(stack[1])
-'''
-print(f'columns: {stackData.pop()}')
-for row in stackData[::-1]:
-    for crate in row[1::4]:
-        print(crate)
-
-
-
-
-for line in stackData:
-    print(line, end='')
-
-'''
-
