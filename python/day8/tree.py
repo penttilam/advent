e3f4564e00684d772552fd93813bbf020f6c6382
mpenttila <michael.penttila@gmail.com>
2023-10-03 15:02:24 -0700

    completed part 2

--- python/day8/tree.py
+++ python/day8/tree.py
@@ -29,3 +29,54 @@ for colNum, col in enumerate(zip(*treeMap)):
             visible.add((colNum,rowNum))
             treeH = int(tree)
 print(f'Number of visible trees are: {len(visible)}')
+
+
+trees={}
+for rowNum, row in enumerate(treeMap):
+    for colNum, tree in enumerate(row):
+        trees.update({(colNum, rowNum):tree})
+        colMx = colNum
+    rowMx = rowNum
+highScr = 0
+for tree in trees:
+    #print(f'{tree}, {tree[0]}, {tree[1]}, {trees[tree]}')
+    treeH = -1
+    viewL = 0
+    for x in range(tree[0]-1, -1, -1):
+        if trees[(x,tree[1])] < trees[tree]:
+            viewL += 1
+        else:
+            viewL += 1
+            break
+    viewR = 0
+    for x in range(tree[0]+1, rowMx+1, 1):
+        if trees[(x,tree[1])] < trees[tree]:
+            viewR += 1
+        else:
+            viewR += 1
+            break
+    viewH = 0
+    for y in range(tree[1]-1, -1, -1):
+        if trees[(tree[0], y)] < trees[tree]:
+            viewH += 1
+        else:
+            viewH += 1
+            break
+    viewB = 0
+    for y in range(tree[1]+1, colMx+1, 1):
+        if trees[(tree[0],y)] < trees[tree]:
+            viewB += 1
+        else:
+            viewB += 1
+            break
+    scr = viewL * viewR * viewH * viewB
+    if scr > highScr:
+        highScr = scr
+
+print(f'The best view has a score of: {highScr}')
+
+
+
+
+
+
